57d0b5eb0d619df2863e0a491b4554d1ee5879a0
theonlypwner <theonlypwner@users.noreply.github.com>
2013-12-04 21:01:14 -0700

    Working tree

--- MainForm.cs
+++ MainForm.cs
@@ -16,12 +16,66 @@ namespace iverson_sigma
             InitializeComponent();
         }
 
+        private readonly Color color_highlight = Color.YellowGreen;
+
         private int[] list;
         private int[][] listProcessed;
 
-        private void recalc()
+        private void Recalc()
         {
-            //listProcessed = new int[list.Length][];
+            // Make the array
+            int n = list.Length;
+            listProcessed = new int[n][];
+            for (int i = 0; i < n; ++i)
+                listProcessed[i] = new int[n - i];
+            // Copy the list
+            for (int j = 0; j < n; ++j)
+                listProcessed[0][j] = list[j];
+            // Process the list
+            for (int gap = 1; gap < n; ++gap)
+                for (int x = 0; x < n - gap; ++x)
+                    listProcessed[gap][x] = Math.Max(list[x] - listProcessed[gap - 1][x + 1],
+                                                    list[x + gap] - listProcessed[gap - 1][x]);
+            // value = listProcessed[n - 1][0]
+        }
+
+        private void MakeTree(TreeNodeCollection parent, int depth, int position = 0, int player = 1)
+        {
+            int val1 = list[position] - listProcessed[depth - 1][position + 1];
+            int val2 = list[position + depth] - listProcessed[depth - 1][position];
+            TreeNode choice1 = new TreeNode(
+                String.Format("P{0} L {1} ({2})", player, list[position], val1)
+            );
+            TreeNode choice2 = new TreeNode(
+                String.Format("P{0} R {1} ({2})", player, list[position + depth], val2)
+            );
+            if (val1 >= val2)
+                choice1.BackColor = color_highlight;
+            if (val2 >= val1)
+                choice2.BackColor = color_highlight;
+            parent.Add(choice1);
+            parent.Add(choice2);
+            if (depth > 1)
+            {
+                MakeTree(choice1.Nodes, depth - 1, position, player ^ 3);
+                MakeTree(choice2.Nodes, depth - 1, position + 1, player ^ 3);
+            }
+        }
+
+        private void MakeTree()
+        {
+            treeViewDecisions.BeginUpdate();
+            treeViewDecisions.Nodes.Clear();
+            TreeNode root = new TreeNode("List value: " + listProcessed[list.Length - 1][0]);
+            treeViewDecisions.Nodes.Add(root);
+            MakeTree(root.Nodes, list.Length - 1);
+            treeViewDecisions.EndUpdate();
+        }
+
+        private void RecalcAndRedraw()
+        {
+            Recalc();
+            MakeTree();
         }
 
         private void MainForm_Load(object sender, EventArgs e)
@@ -33,7 +87,7 @@ namespace iverson_sigma
         {
             ArrayList numbers_int = new ArrayList();
             ArrayList numbers_str = new ArrayList();
-            
+
             // Get valid numbers
             foreach (string s in txtInput.Text.Split(' '))
             {
@@ -50,7 +104,7 @@ namespace iverson_sigma
 
             // Recalculate the list
             list = (int []) numbers_int.ToArray(typeof(int));
-            recalc();
+            RecalcAndRedraw();
         }
     }
 }
